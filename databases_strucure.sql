b689fa9c011ab6bb05a0c54588e61b12ce65791b
SYHRL360 <not.syahrul@outlook.co.id>
2026-04-18 22:07:42 +0700

    Fixed sql script database creation for kafka_log table

--- databases_strucure.sql
+++ databases_strucure.sql
@@ -40,8 +40,8 @@ CREATE TABLE IF NOT EXISTS `kafka_log` (
   `id` int NOT NULL AUTO_INCREMENT,
   `service_receiver` varchar(50)  NULL,
   `topic_name` varchar(50) NULL,
-  `group_id` varchar(50) NULL
-  `content` MEDIUMTEXT  NULL,
+  `group_id` varchar(50) NULL,
+  `content` text  NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
